c4a096da23bae61dde1ede0ac710ea8566780bb0
AustinTD4 <101255991+AustinTD4@users.noreply.github.com>
2023-12-04 10:51:03 +0100

    Clock Manager
    This object allows the training process to analyze which portions are taking the most time to compute

---
+++ MCTS_Connect_Four/clockManager.py
@@ -0,0 +1,55 @@
+import time
+import pickle
+
+class timer:
+
+    def __init__(self):
+        self.netTime = 0
+        self.windows = 0
+        
+    def start(self):
+        self.startTime = time.monotonic_ns()
+
+    def stop(self):
+        self.netTime += time.monotonic_ns() - self.startTime
+        self.windows += 1
+
+class clockManager:
+
+    def __init__(self, fileName):
+        self.fileName = fileName
+        self.start = time.monotonic_ns()
+        self.timers = {}
+
+    def makeTimer(self, name):
+        self.timers[name] = timer()
+        return self.timers[name]
+
+    def tallyCompare(self):
+        duration = time.monotonic_ns() - self.start
+        timeTotals = []
+        timerNames = []
+
+        for clock in self.timers.keys():
+            timerNames.append(clock)
+            timeTotals.append(self.timers[f'{clock}'].netTime)
+            print(f'{clock} used a total of {timeTotals[-1]} nanoseconds, {round(timeTotals[-1]/self.timers[f"{clock}"].windows,3)} per window')
+
+        netFractions = []
+        relativeFractions = []
+        for i in range(len(timeTotals)):
+            netFraction = timeTotals[i]/duration
+            relativeFraction = timeTotals[i]/sum(timeTotals)
+            netFractions.append(netFraction)
+            relativeFractions.append(relativeFraction)
+            print(f'{timerNames[i]} took {round(netFraction,3)} of total time and {round(relativeFraction,3)} of timed processes')
+
+        results = {}
+        results['Timer Names'] = timerNames
+        results['Time Totals'] = timeTotals
+        results['Net Fractions'] = netFractions
+        results['Relative Fractions'] = relativeFractions
+
+        with open(f'{self.fileName}_Timers.pkl', 'wb') as file:
+            pickle.dump(results, file, protocol=pickle.HIGHEST_PROTOCOL)
+
